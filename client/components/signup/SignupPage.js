1b9c097d2a34e7659f1a9904be1eb7766e85b65b
mlamont8 <debiandmiat@hotmail.com>
2017-03-31 15:26:21 -0400

    Sign up form and its state

--- client/components/signup/SignupPage.js
+++ client/components/signup/SignupPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import SignupForm from './SignupForm';
 
 
 export default React.createClass({
 
   render() {
     return (
-      <div>Sign Up Page</div>
+      <div className="row">
+        <div className="col-md-4 col-md-offest-4">
+          <SignupForm />
+        </div>
+      </div>
+
     );
   }
 
