628763e237d9b8c45d087084c49548fb3cf4cb40
mlamont8 <debiandmiat@hotmail.com>
2017-04-01 21:19:30 -0400

    clientside validation

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,10 @@ export default {
     loaders: [
       {
         test: /\.js$/,
-        include: path.join(__dirname, 'client'),
+        include:
+        [path.join(__dirname, 'client'),
+        path.join(__dirname, 'server/shared')
+      ]
         loaders: [ 'babel-loader']
       }
     ]
